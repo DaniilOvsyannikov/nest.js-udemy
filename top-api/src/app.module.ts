410e730ab6d0ec21ac6de186eaf174ed6c0c139b
Даниил Овсянников <76049207+DaniilOvsyanniukov@users.noreply.github.com>
2023-08-31 16:52:55 +0300

    add model

--- top-api/src/app.module.ts
+++ top-api/src/app.module.ts
@@ -5,9 +5,26 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ReviewModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/test'),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }),
+    AuthModule,
+    TopPageModule,
+    ReviewModule,
+    ProductModule,
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- top-api/src/top-page/top-page.model.ts
+++ top-api/src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop } from "@typegoose/typegoose";
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+
 export enum TopLevelCategory {
 	Courses,
 	Sevices,
@@ -5,23 +8,57 @@ export enum TopLevelCategory {
 	Products
 }
 
-export class TopPageModel {
-	_id: string;
-	firstCategory: TopLevelCategory;
-	secondCategory: string;
+export class hhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSelary: number;
+
+	@prop()
+	middleSelary: number;
+
+	@prop()
+	seniorSelary: number;
+}
+
+export class topPageAdvantages {
+	@prop()
 	title: string;
+
+	@prop()
+	description: string;
+}
+
+export interface TopPageModel extends Base { }
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopLevelCategory })
+	firstCategory: TopLevelCategory;
+
+	@prop()
+	secondCategory: string;
+
+	@prop({ unique: true })
+	alias: string;
+
+	@prop()
+	title: string;
+
+	@prop()
 	category: string;
-	hh?: {
-		count: number;
-		juniorSelary: number;
-		middleSelary: number;
-		seniorSelary: number;
-	};
-	advantages: {
-		title: string;
-		description: string;
-	}[];
+
+	@prop({ type: () => hhData })
+	hh?: hhData;
+
+	@prop({ type: () => [topPageAdvantages] })
+	advantages: topPageAdvantages[];
+
+	@prop()
 	seoText: string;
+
+	@prop()
 	tagsTitle: string;
+
+	@prop({ type: () => [String] })
 	tags: string[];
 }
